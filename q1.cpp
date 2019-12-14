498384ffde2aafc482b0dfc03e374b58a59f12ed
Aniket Raj <aniketraj.go@gmail.com>
2019-12-14 22:48:10 +0530

    Print natural num upto N without using semi-colon

---
+++ q1.cpp
@@ -0,0 +1,21 @@
+//Print all natural numbers upto N without using semi-colon in main program
+
+#include<iostream>
+
+//#define n 11
+
+using namespace std;
+
+int n=10;
+
+int main(){
+
+
+
+while((cout<<(11-n)<<"\t") && (n=n-1))
+
+{}
+
+
+
+}
