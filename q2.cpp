7cdc989d271b2a390fe69a31db56b6bd7c6c5e2d
Aniket Raj <aniketraj.go@gmail.com>
2019-12-14 22:54:25 +0530

    Create q2.cpp

---
+++ q2.cpp
@@ -0,0 +1,18 @@
+//divide an integer by 4 without using ‘/’ operator.
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+   int n = 10;
+
+   n = n >> 2; // 0010----> 0100
+
+   cout << n;
+
+   return 0;
+
+}
