e49e367b45ece60bdb3438087229428c37959166
Aniket Raj <aniketraj.go@gmail.com>
2019-12-15 23:43:37 +0530

    Add files via upload

---
+++ q3.cpp
@@ -0,0 +1,12 @@
+//Program to print numbers from 1 to 10 with using loops or goto statements
+#include <stdio.h>
+void printNumber(int count){
+   printf("%d\n", count );
+   count+=1;
+   if(count<=10)
+      printNumber(count);
+}
+int main(){
+   printNumber(1);
+   return 0;
+}
